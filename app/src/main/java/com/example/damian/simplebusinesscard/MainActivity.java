944b17940e289945f93291f5230ab123dad732cf
damwilin <damian.wilinski@gmail.com>
2017-04-12 17:09:06 +0200

    1. Added opening hours 2. Added location with Google Maps Intent 3. Cleaned code 4. Deleted unused images 5. Added icons

--- app/src/main/java/com/example/damian/simplebusinesscard/MainActivity.java
+++ app/src/main/java/com/example/damian/simplebusinesscard/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.damian.simplebusinesscard;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showMapIntent(View view) {
+        Uri gmmIntentUri = Uri.parse("geo:0,0?q=54.261500,16.052973(U Rybaka)");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
     }
 }
